eee5053ee9a0c11227cba8b7fd0f14c686aa9025
castellanos70@gmail.com <castellanos70@gmail.com>
2023-07-23 01:32:29 -0600

    fixed many resourece pack errors

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ Minecraft Datapack bringing Creativity, Intrest, Challange, Fun and Balance
 ## Dev Notes:
 ### Creating Resource Pack, BedrockCreeper_RP_x.y.z.zip
 1. The TOP LEVEL of the `.zip` must have `assets` folder, `pack.mcmeta`, `pack.png`, and `Selected Packs.txt` files.
-2. Into a temporary working folder, tmp, which is ignored by git, copy the contents of `resource_pack\BedrockCreeper_vanilla-tweaks`. Next, merge into the blocks and textures directories `resource_pack\BedrockCreeper_blocks` and `resource_pack\BedrockCreeper_textures`.
-3. Copy `data_pack\pack.png`, update version and copy `data_pack/pack.mcmeta` into tmp folder.
-4. Zip tmp folder contains (not the folder itself) and upload to website. 
-5. Update server preference file to the new .zip file name with new version number
+2. Into a temporary working folder, tmp, which is ignored by git, copy the contents of `resource_pack\BedrockCreeper_vanilla-tweaks`. Note that `resource_pack\BedrockCreeper_vanilla-tweaks` is *not* a simple unzip of
+what vanilla-tweaks downloads. In particular, the .json model files for warped and crimson planks, steps and slabs using 6 texture variations need to be deleated (and the corrosponding textues have also been deleated)
+3. Next, merge into the blocks and textures directories `resource_pack\BedrockCreeper_blocks` and `resource_pack\BedrockCreeper_textures`.
+4. Copy `data_pack\pack.png`, update version and copy `data_pack/pack.mcmeta` into tmp folder.
+5. Zip tmp folder contains (not the folder itself) and upload to website. 
+6. Update server preference file to the new .zip file name with new version number
 
--- resource_pack/BedrockCreeper_vanilla-tweaks/assets/minecraft/shaders/include/light.glsl
+++ resource_pack/BedrockCreeper_vanilla-tweaks/assets/minecraft/shaders/include/light.glsl
@@ -16,21 +16,30 @@ vec4 minecraft_sample_lightmap(sampler2D lightMap, ivec2 uv) {
 	float blockLight = uv.x / 16.0;
 	float skyLight = uv.y / 16.0;
 	
+	vec4 pureSkyLight = texture(lightMap, clamp(vec2(0.0, 240.0) / 256.0, vec2(0.5 / 16.0), vec2(15.5 / 16.0))); // Gets the color of pure skylight.
+	float dayness = 1.0 - (smoothstep(7.0, 15.0, skyLight) * smoothstep(0.164, 0.976, pureSkyLight.r));
+	
+	float brightnessModifier = ((blockLight * dayness) + 32.0) / 32.0; // Lighter light levels are made brighter because it looks pretty.
+	vec3 warmTint = vec3(0.15, 0.04, -0.15) * dayness; // The default warm tint of light.
+	vec3 lightModifier = vec3(brightnessModifier) + (warmTint * smoothstep(0.0, 7.0, blockLight)); // Combine the brightness modifier and the warm tint to get the nice glow effect!
+	
 	float darknessMultiplier = ((blockLight / 10.0) + 0.3) * smoothstep(0.0, 7.0, blockLight); // Darker light levels are made more dark for a smoother transition.
 	vec4 darkColor = vec4(darknessMultiplier, darknessMultiplier, darknessMultiplier, 1.0); // The "color" of the cave darkness.
 	
 	vec4 defaultLightColor = texture(lightMap, clamp(uv / 256.0, vec2(0.5 / 16.0), vec2(15.5 / 16.0))); // Gets what the light color would be in vanilla.
-	vec4 pureSkyLight = texture(lightMap, clamp(vec2(0.0, 240.0) / 256.0, vec2(0.5 / 16.0), vec2(15.5 / 16.0))); // Gets the light color at block light 0, sky light 15.
 	vec4 light0 = texture(lightMap, clamp(vec2(0.0, 0.0) / 256.0, vec2(0.5 / 16.0), vec2(15.5 / 16.0))); // Gets the light color at light level 0.
+	
+	vec4 newLightColor = vec4(1.0, 1.0, 1.0, 1.0); // Define the variable so it can be modified later without it throwing a fit.
 	
 	float nightness = smoothstep(0.1, 0.25, pureSkyLight.r); // Gets how close to night it should be. Also helps with compatibility for different brightnesses.
 	vec4 nightColor = (vec4(0.2, 0.225, 0.275, 1.0) + vec4(nightness, nightness, nightness, 1.0)) * smoothstep(9.0, 16.0, (16.0 - blockLight)); // The darkness the game adds at night.
 	nightColor = nightColor.r + darkColor.r > 1.0 ? vec4(1.0, 1.0, 1.0, 1.0) - vec4(darkColor.rgb, 0.0) : nightColor; // Prevents the night color from making the game brighter instead of darker. I speak from experience.
 	nightness = 1.0 - nightness; // Flip nightness after its purpose in math is finished, to make it more human understandable.
 	
-	if (blockLight < 8.0 && light0.r > 0.5) blockLight = 8.0; // If it's that bright at light level 0, you probably have night vision on.
+	if (blockLight > 0.0) newLightColor = defaultLightColor * vec4(lightModifier, 1.0); // If there is enough block light, add the light modifier.
+	else newLightColor = defaultLightColor; // If there is no block light, set the new color to default.
 	
-	vec4 newLightColor = defaultLightColor;
+	if (blockLight < 8.0 && light0.r > 0.5) blockLight = 8.0; // If it's that bright at light level 0, you probably have night vision on.
 	
 	if (blockLight == 0.0 && skyLight == 0.0) newLightColor = vec4(0.0, 0.0, 0.0, 1.0); // If there's no light, make it pitch black. A bit redundant after smoothing darknessMultiplier but I'm just making sure.
 	else if (blockLight <= 7.0 && skyLight == 0.0) newLightColor *= darkColor; // If there is low block light and there is no sky light, make it darker.
